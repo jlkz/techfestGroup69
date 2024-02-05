c7b9dac7212fe1c4faefe16c8b3458821f45f5b0
Shen Hwei Yap <81760484+imaginaryBuddy@users.noreply.github.com>
2024-02-05 22:12:42 +0800

    Add FindMatch

--- frontend/src/pages/AfterMatch/index.js
+++ frontend/src/pages/AfterMatch/index.js
@@ -1,37 +1,75 @@
 import React, { useState, useEffect } from "react";
 import { Card, Button, Space, Tag } from "antd";
+import { useNavigate } from "react-router-dom";
+import MatchProfile from "./MatchProfile";
 
 export default function AfterMatch(props) {
-  const { matchesData, setMatchesData } = useState([]);
-  const { openChatBot, setOpenChatBot } = useState(false);
-  const { generateCaption, setGenerateCaption } = useState(false);
+
+  const  [successfulMatchesData, setSuccessfulMatchesData]  = useState([]);
+  const [usersData, setUsersData]  = useState([]);
+  const [filteredData, setFilteredData]  = useState([]);
+  const  [openChatBot, setOpenChatBot]  = useState(false);
+  const  [generateCaption, setGenerateCaption]  = useState(false);
+  
+  const navigate = useNavigate(); 
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const response = await fetch("matches.json"); 
+        const data = await response.json(); 
+        const matchesData = data.userMatches; 
+        console.log("matchesData", matchesData);
+        setSuccessfulMatchesData(matchesData.successfulMatchesWithUser); 
+        console.log("successfulMatchesData", matchesData.successfilMatchesWithUser);
+        const response2 = await fetch("users.json");
+        const data2 = await response2.json(); 
+        setUsersData(data2.users); 
+        console.log("AFTERMATCH: this is usersData", usersData);
+
+        setFilteredData(usersData.filter(user => successfulMatchesData.includes(user.id)));
+
+
+        
+      } catch (error) {
+        console.error("AFTERMATCH: Error fetching data: ", error);
+      }
+    };
+    fetchData(); 
+  }, []);
+
+
+  const handleOpenChatBot = () => {
+    setGenerateCaption(false);
+    setOpenChatBot(true); 
+    console.log("handleOpenChatBot");
+    navigate('/ChatBot');
+  }
+
+  const handleGenerateCaption = () => {
+    setOpenChatBot(false);
+    setGenerateCaption(true); 
+    console.log("handleGenerateCaption");
+    navigate('/ImageCaption');
+  }
 
   return (
     <>
       <div className="flex flex-col items-center">
         <h2 className="justify-center ">You guys matched!</h2>
-        <Card className="m-auto"
-          style={{ width: 300 }}
-          cover={
-            <img
-              alt="displayName"
-              src="https://via.assets.so/movie.png?id=1&q=95&w=360&h=360&fit=fill"
-            />
-          }
-          title="title"
-        >
-            
-          
-        </Card>
-        <Space className="m-3" align="center" direction="vertical" size="large">
-                <Button>
-                    Ask ChatBot to Suggest Activities!
-                </Button>
-                <Button>
-                    Make your photo captions fun!
-                </Button>
-            </Space>
-        </div>
+        {filteredData && filteredData.map((fd) => {
+          return <MatchProfile
+            displayName={fd.displayName}
+            interests={fd.interests}
+            values={fd.values}
+            university={fd.university}
+            age={19}
+            profilePicture={fd.profilePicture}
+            handleGenerateCaption={handleGenerateCaption}
+            handleOpenChatBot={handleOpenChatBot}
+          ></MatchProfile>
+        })}
+      </div>
     </>
   );
 }
